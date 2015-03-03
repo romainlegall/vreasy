0b0bd99d2db2b5a36fed42507fd9cc86ba065707
mbpderomainlegall <romain@bigsquares.com>
2015-03-03 09:24:07 +0100

    documentation

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,32 +1,41 @@
 class MessagesController < ApplicationController
 
 	# Twilio configuration
-	$account_sid = ''
-	$auth_token = ''
+	$account_sid = 'ACb88d7015c3b1d42a1343125b0ab21508'
+	$auth_token = '14136e72b550b54502adc9612e7b1bec'
 
 	# index page
 	def index
 		@message = Message.new
 		@messages = Message.all	
-		check_msgs #check status of all messages
 	end
 	
 	# Creation of a new message
 	def create
 		params[:message][:ref] = SecureRandom.hex(10) #generate a new ref
-		params[:message][:sid] = twilio_new_msg(params[:message][:from], params[:message][:to], params[:message][:body],  params[:message][:ref]) #call twilio_new_msg
-		if params[:message][:sid].blank?
-			params[:message][:status] = "fail" 
-		end
+		result = twilio_new_msg(params[:message][:from], params[:message][:to], params[:message][:body], params[:message][:ref]) #call twilio_new_msg
+		params[:message][:sid] = result[0] unless result.blank?
+		puts params[:message][:status] = result[1] unless result.blank?
 		@message = Message.new(message_params)
 		@message.save		
 		index
+	end
+	
+
+	# check all messages informations
+	def check_msgs
+		messages = Message.all	
+		messages.each do |m|
+			status = get_msg_info(m.sid)
+			m.status = status unless status=="Error" and m.status != "Error"
+			m.save
+		end
 	end
 
 	private
 	# params acceptable
 	def message_params
-		params.require(:message).permit(:from, :to, :body, :ref, :status)
+		params.require(:message).permit(:from, :to, :body, :ref, :status, :sid)
 	end	
 
 	#twilio calling for new message
@@ -40,7 +49,7 @@ class MessagesController < ApplicationController
 			    :from => p_from,
 			   	:media_url => "http://localhost:3000/#{p_ref}"
 			   ) 
-			message.sid
+			return [message.sid, message.status]
 		rescue
 
 		end
@@ -48,6 +57,7 @@ class MessagesController < ApplicationController
 
 	#twilio calling for get info of a message
 	def get_msg_info(sid)
+
 		begin
 			# Get your Account Sid and Auth Token from twilio.com/user/account
 			client = Twilio::REST::Client.new $account_sid, $auth_token
@@ -55,27 +65,16 @@ class MessagesController < ApplicationController
 			# Get an object from its sid. If you do not have a sid,
 			# check out the list resource examples on this page
 			message = client.account.messages.get(sid)
-			message.StatusCallback
-			if ["Ok", "Yes", "Si"].included_in?(message.response.message)
-
-			elsif ["No"].included_in?(message.response.message)
-
+			if  message.response.message.downcase.split.include?("yes") or message.downcase.response.message.split.include?("si") or message.downcase.response.message.split.include?("ok")
+				status = "accepted"
+			elsif  message.response.message.downcase.split.include?("no") 
+				status = "refused"
 			else
-
+				status = "Unknown"
 			end
+			return status
 		rescue
-
-		end
-	end
-
-	# check all messages informations
-	def check_msgs
-		messages = Message.all	
-		messages.each do |m|
-			puts m.ref
-			status = get_msg_info(m.ref)
-			m.status = status
-			m.save
+			return "Error"
 		end
 	end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root to: 'messages#index'
   resources :messages
+  get 'check_msgs' => 'messages#check_msgs'
 end
